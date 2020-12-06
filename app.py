9117cb5331be982e3137dfecbe3a2bbe3dc39e2a
piotrpietrzyk <piotrek.pietrzyk@gmail.com>
2020-12-06 23:18:53 +0100

    [edit] add argparse to app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from valuation import valuation
 import os
-
-
+import argparse
 cwd = os.getcwd()
-valuation = valuation.Valuation(cwd+'/data/data.csv', cwd+'/data/currencies.csv', cwd+'/data/matching.csv')
-valuation.converse_currency()
-valuation.total_price()
-valuation.sort_values()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--currencies', '-c', type=os.path.abspath, help='path to currencies csv')
+parser.add_argument('--data', '-d', type=os.path.abspath, help='path to data csv')
+parser.add_argument('--matching', '-m', type=os.path.abspath, help='path to matching csv')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    valuation = valuation.Valuation(args.data, args.currencies, args.matching)
+    valuation.converse_currency()
+    valuation.total_price()
+    valuation.sort_values()
+    valuation.top_product()
+    valuation.save_csv(f'{cwd}/data/top_products.csv')
 
--- valuation/valuation.py
+++ valuation/valuation.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+pd.options.mode.chained_assignment = None
+
 
 class Valuation(object):
     def __init__(self, data, currencies, matchings):
@@ -22,3 +24,22 @@ class Valuation(object):
     def sort_values(self):
         self.data = self.data.sort_values(by=['matching_id', 'total_price'], ascending=False)
 
+    def top_product(self):
+        copy = self.data.copy()
+        for index, row in self.matchings.iterrows():
+            self.data[self.data['matching_id'] == row['matching_id']] = self.data[self.data['matching_id'] == row[
+                'matching_id']].iloc[0:row['top_priced_count']]
+        self.data = self.data.dropna()
+        self.data = self.data.reset_index()
+        self.top_products['total_price'] = self.data.groupby('matching_id')['total_price'].sum()
+        self.top_products['avg_price'] = self.data.groupby('matching_id')['total_price'].mean()
+        self.top_products['currency'] = self.data['currency']
+        self.top_products.insert(loc=0, column='matching_id', value=self.top_products.index)
+        self.top_products['ignored_products_count'] = np.nan
+        for index, row in self.top_products.iterrows():
+            self.top_products['ignored_products_count'].loc[index] = len(
+                copy[copy['matching_id'] == row['matching_id']]) - len(
+                self.data[self.data['matching_id'] == row['matching_id']])
+
+    def save_csv(self, path):
+        self.top_products.to_csv(path, index=False)
