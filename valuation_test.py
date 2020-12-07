724fc97997ca55d2bb7239bb94e4259eb1cbe6ef
piotrpietrzyk <piotrek.pietrzyk@gmail.com>
2020-12-07 12:30:36 +0100

    [wip] update tests valuation.py

--- valuation_test.py
+++ valuation_test.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 from valuation import valuation
+import pandas as pd
 
 cwd = os.getcwd()
 
@@ -28,4 +29,20 @@ def test_total_price(val):
         assert row['total_price'] == row['price'] * row['quantity']
 
 
+@pytest.mark.parametrize("matching_id",
+                         (1, 2, 3))
+def test_sort_values(val, matching_id):
+    val.converse_currency()
+    val.total_price()
+    val.sort_values()
+    values_list = []
+    assert pd.Index(val.data['matching_id']).is_monotonic_decreasing
+    for index, row in val.data.iterrows():
+        if row['matching_id'] == matching_id:
+            values_list.append(row['total_price'])
 
+    assert values_list == sorted(values_list, reverse=True)
+
+
+def test_top_product():
+    pass
